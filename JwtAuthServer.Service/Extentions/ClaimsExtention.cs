4fa29f40708484f3de97f953c48c56f6c19d42ff
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-08-15 00:28:07 +0300

    Set Claims

---
+++ JwtAuthServer.Service/Extentions/ClaimsExtention.cs
@@ -0,0 +1,47 @@
+﻿using JwtAuthServer.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthServer.Service.Extentions
+{
+    public static class ClaimsExtention
+    {
+        public static void SetName(this List<Claim> claims,string name)
+        {
+            claims.Add(new Claim(ClaimTypes.Name, name));
+        }
+        public static void SetNameIdentifier(this List<Claim>claims,string id)
+        {
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, id));
+        }
+        public static void SetEmail(this List<Claim>claims,string email)
+        {
+            claims.Add(new Claim(ClaimTypes.Email, email));
+        }
+        public static void SetRoles(this List<Claim> claims, string[] roles)
+        {
+            roles.ToList().ForEach(p => claims.Add(new Claim(ClaimTypes.Role, p)));
+        }
+        public static void SetAudiences(this List<Claim> claims, string[] audiences)
+        {
+            audiences.ToList().ForEach(audience => claims.Add(new Claim(JwtRegisteredClaimNames.Aud, audience)));
+        }
+        public static void SetJti(this List<Claim>claims,string jti)
+        {
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, jti));
+        }
+        public static void SetSub(this List<Claim>claims,string sub)
+        {
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, sub));
+        }
+        public static void SetAudiencesByClient(this List<Claim> claims,Client client)
+        {
+            client.Audiences.ForEach(audience => claims.Add(new Claim(JwtRegisteredClaimNames.Aud, audience)));
+        }
+    }
+}
---
+++ JwtAuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,68 @@
+﻿using JwtAuthServer.Core.Configuration;
+using JwtAuthServer.Core.Dtos;
+using JwtAuthServer.Core.Models;
+using JwtAuthServer.Core.Services;
+using JwtAuthServer.Service.Extentions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly CustomTokenOption _tokenOption;
+        private readonly UserManager<UserApp> _userManager;
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> options)
+        {
+            _userManager = userManager;
+            _tokenOption = options.Value;
+        }
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+            using (var random = RandomNumberGenerator.Create())
+            {
+                random.GetBytes(numberByte);
+            }
+            return Convert.ToBase64String(numberByte);
+        }
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateClientTokenDto(Client client)
+        {
+            throw new NotImplementedException();
+        }
+        private List<Claim> SetClaims(UserApp userApp, List<string> audiences)
+        {
+            var claims = new List<Claim>();
+            var roles = _userManager.GetRolesAsync(userApp).Result.ToArray();
+            claims.SetName(userApp.UserName);
+            claims.SetEmail(userApp.Email);
+            claims.SetNameIdentifier(userApp.Id);
+            claims.SetRoles(roles);
+            claims.SetAudiences(audiences.ToArray());
+            claims.SetJti(Guid.NewGuid().ToString());
+            return claims;
+        }
+
+        private List<Claim>GetClaimsByClient(Client client)
+        {
+            var claims = new List<Claim>();
+            claims.SetAudiencesByClient(client);
+            claims.SetJti(Guid.NewGuid().ToString());
+            claims.SetSub(client.ClientId);
+            return claims;
+        }
+    }
+}
