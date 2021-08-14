db7a15265817f9c10083efbd7a98972ecca6488b
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-08-14 14:31:43 +0300

    Add project files.

---
+++ JwtAuthServer.Core/Services/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthServer.Core.Services
+{
+    public interface IGenericService<TEntity, TDto> where TEntity : class, new() where TDto : class, new()
+    {
+        Task<Response<TDto>> GetByIdAsync(int id);
+        Task<Response<IEnumerable<TDto>>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        Response<TDto> Update(TEntity entity);
+        Response<IEnumerable<TDto>> Where(Expression<Func<TEntity, bool>> expression);
+    }
+}
